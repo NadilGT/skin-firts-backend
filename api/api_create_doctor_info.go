339007c2dfe581e575303dcaa3741cede95be5d4
NadilGT <rccnadildinsara@gmail.com>
2026-03-09 13:11:10 +0530

    feat: Add doctor information model and API endpoint for creating doctor profiles.

--- api/api_create_doctor_info.go
+++ api/api_create_doctor_info.go
@@ -1,14 +1,21 @@
 package api
 
 import (
+	"context"
 	"lawyerSL-Backend/dao"
 	"lawyerSL-Backend/dto"
+	"lawyerSL-Backend/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func CreateDoctorInfo(c *fiber.Ctx) error {
 	var info dto.DoctorInfoModel
+	id, err := dao.GenerateId(context.Background(), "doctor_info", "DOC")
+	if err != nil {
+		return utils.SendErrorResponse(c, fiber.StatusInternalServerError, err.Error())
+	}
+	info.DoctorID = id
 	if err := c.BodyParser(&info); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Invalid request body",
